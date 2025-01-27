67abc3e1fc007e7cd53b91de3406ea7f669366fd
Alireza Abolghasempour <161477999+2AWebDeveloper@users.noreply.github.com>
2025-01-27 16:19:53 +0330

    Fixed problem ...

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,8 @@ const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
 const loaderEl = document.querySelector(".loader");
 
-
 // Variables
 let nowDate, timeFormat;
-
 
 // Functions
 const handleZero = function(number) {
@@ -21,40 +19,28 @@ const handleZero = function(number) {
         return number;
     }
 };
-
 const handleTimeFormat = function(number) {
     if (number > 12) {
         timeFormat = "PM";
-        return `0${number - 12}`;
+        return number - 12;
     } else {
         timeFormat = "AM";
         return number;
     }
 };
-
 const updateTime = function() {
-
     setInterval(() => {
-
         nowDate = new Date();
-
         needleHoursEl.style.transform = `translate(-50%, -100%) rotate(${(nowDate.getHours() - 12) * 30}deg)`;
-
         needleMinuteEl.style.transform = `translate(-50%, -100%) rotate(${nowDate.getMinutes() * 6}deg)`;
-
         needleSecondEl.style.transform = `translate(-50%, -100%) rotate(${nowDate.getSeconds() * 6}deg)`;
-
         timeEl.innerHTML = `
-            ${handleTimeFormat(handleZero(nowDate.getHours()))}:${handleZero(nowDate.getMinutes())}
+            ${handleZero(handleTimeFormat(nowDate.getHours()))}:${handleZero(nowDate.getMinutes())}
             <span>${timeFormat}</span>
         `;
-
         dateEl.innerHTML = `${nowDate.getDate()} ${nowDate.toLocaleDateString("en", {month: "short"})}, ${nowDate.getFullYear()}`;
-        
     }, 1000);
-
 };
-
 const updateClock = function() {
     updateTime();
     setTimeout(() => {
